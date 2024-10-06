3f3f7128b54afe0cfcedac8772e45306939278d5
Fabian Galán <180666585+xkodigo1@users.noreply.github.com>
2024-10-06 13:04:49 -0500

    Configuracion Usuario Vs Entorno V.2 (Agregué varios detalles e hice ciertas correciones para que el modo de juego mejorara su funcionamiento. Implementé un marcador que se muestra durante cada ronda. Además de que cuando el usario o el entorno llegue a los 3 puntos, se termine la partida e imprima al final quién ganó)

--- modules/menuP.py
+++ modules/menuP.py
@@ -13,32 +13,42 @@ def menuP():
     input("¡Bienvenido a Rock, Paper N' Scissors! ¿Preparado para poner a prueba tu suerte mediante una elecciòn?\nPresiona Enter para continuar --> ")
     opc_user = int(input("Es hora de elegir còmo quieres jugar. Elige una opcion:\n1). PvE (Jugador vs Entorno)\n2). JcJ (Jugador contra Jugador)\n--> "))
     if opc_user == 1:
+        print("Antes de comenzar, es necesario registrarse.")
+        rgstr_user = input("Ingrese su nombre de usuario: ")
+        users = {
+            'name' : rgstr_user
+        }
+        input("Usuario registrado con éxito. Presiona Enter para continuar --> ")
         os.system("cls")
         while True:
-            print("Antes de comenzar, es necesario registrarse.")
-            rgstr_user = input("Ingrese su nombre de usuario: ")
-            users = {
-                'name' : rgstr_user
-            }
-            input("Usuario registrado con éxito. Presiona Enter para continuar --> ")
-            os.system('cls')
-            user = input("Elige una opción:\n--> Piedra\n--> Papel\n--> Tijeras\n ").capitalize()
-            opc = ["Piedra","Papel","Tijeras"]
-            # if user != "Piedra" or user != "Papel" or user != "Tijeras": return print("Elección no válida, intentalo de nuevo.")
-            env = random.choice(opc)
-            if user == "Piedra" and env == "Tijeras":
-                print(f"¡{user} aplasta {env}! ¡Lo hiciste genial, esta ronda es tuya!")
-            elif user == "Papel" and env == "Piedra":
-                print(f"¡{user} envuelve {env}! ¡Muy bien, esta vez ganaste tú!")
-            elif user == "Tijera" and env == "Papel":
-                print(f"¡{user} corta {env}! ¡Buena jugada, te llevas la victoria!")
-            elif env == "Piedra" and user == "Tijeras":
-                print(f"¡{env} aplasta {user}! Esta vez no fue la tuya, ¡pero la próxima seguro lo es!")
-            elif env == "Papel" and user == "Piedra":
-                print(f"¡{env} envuelve {user}! Has perdido esta vez.")
-            elif env == "Tijera" and user == "Papel":
-                print(f"¡{env} corta {user}! No te preocupes, ¡a la siguiente lo conseguirás!")
+            if cntdr["rnds_gu"] < 3 and cntdr["rnds_ge"] < 3:
+                user = input("---------------------------------\nElige una opción:\n--> Piedra\n--> Papel\n--> Tijeras\n---------------------------------\n").capitalize()
+                opc = ["Piedra","Papel","Tijeras"]
+                env = random.choice(opc)
+                if user == "Piedra" and env == "Tijeras":
+                    cntdr["rnds_gu"] += 1
+                    print(f"¡{user} aplasta {env}! ¡Lo hiciste genial, esta ronda es tuya!\nMARCADOR: {cntdr["rnds_gu"]} - {cntdr["rnds_ge"]}")
+                elif user == "Papel" and env == "Piedra":
+                    cntdr["rnds_gu"] += 1
+                    print(f"¡{user} envuelve {env}! ¡Muy bien, esta vez ganaste tú!\nMARCADOR: {cntdr["rnds_gu"]} - {cntdr["rnds_ge"]}")
+                elif user == "Tijera" and env == "Papel":
+                    cntdr["rnds_gu"] += 1
+                    print(f"¡{user} corta {env}! ¡Buena jugada, te llevas la victoria!\nMARCADOR: {cntdr["rnds_gu"]} - {cntdr["rnds_ge"]}")
+                elif env == "Piedra" and user == "Tijeras":
+                    print(f"¡{env} aplasta {user}! Esta vez no fue la tuya, ¡pero la próxima seguro lo es!\nMARCADOR: {cntdr["rnds_gu"]} - {cntdr["rnds_ge"]}")
+                    cntdr["rnds_ge"] += 1
+                elif env == "Papel" and user == "Piedra":
+                    print(f"¡{env} envuelve {user}! Has perdido esta vez.\nMARCADOR: {cntdr["rnds_gu"]} - {cntdr["rnds_ge"]}")
+                    cntdr["rnds_ge"] += 1
+                elif env == "Tijera" and user == "Papel":
+                    print(f"¡{env} corta {user}! No te preocupes, ¡a la siguiente lo conseguirás!\nMARCADOR: {cntdr["rnds_gu"]} - {cntdr["rnds_ge"]}")
+                    cntdr["rnds_ge"] += 1
+                elif user == env:
+                    print("¡Empate! Ambos elegimos lo mismo. ¡Parece que están en sintonía! ¿Vamos de nuevo?")
+                elif user != opc:
+                    input("Elección no válida. Presiona Enter para regresar --> ")
+            else: 
+                print(f"{users["name"]} es el ganador" if cntdr["rnds_gu"] > cntdr["rnds_ge"] else "Entorno es el ganador")
+                break
     if opc_user == 2:
         pass
-
-    
